72cbd5b31b29007333cb434e4567b8fdacfa82f3
dreamstu <cjxsgn@163.com>
2015-01-28 19:24:41 +0800

    build

---
+++ qconf.js
@@ -0,0 +1,16 @@
+/**
+ * Created by johnkim on 1/9/15.
+ */
+module.exports={
+    //配置待构建路径
+    //logFile:'logs',
+    build:{
+        //logFile:'build_log',
+        path:'/home/johnkim/Documents/git/org-quickjs-app/src/main/webapp/src/js/seajs-module/static/page',
+        //filter:/^(node_modules|jquery|seajs|logs)$/i
+    },
+    prefix:'gallery',
+    serve:{
+        root:'/home/johnkim/Documents/git/org-quickjs-app/src/main/webapp/src/js/seajs-module'
+    }
+};
